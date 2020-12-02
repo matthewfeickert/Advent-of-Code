818aa3e4947b5fb2e91159c4880419ec67ff28ae
Matthew Feickert <matthew.feickert@cern.ch>
2020-12-02 16:47:38 -0600

    Add day3 template

---
+++ 2020/day-03/day-03.md
@@ -0,0 +1,9 @@
+# Day 3: XXX
+
+## Part One
+
+Text
+
+## Part Two
+
+Text
---
+++ 2020/day-03/python/day-03.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+
+def load_input_file(path):
+    with open(path) as input_file:
+        return [line.strip() for line in input_file]
+
+
+test_data = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]
+
+
+def test_part_one():
+    pass
+
+
+def part_one():
+    # inputs = load_input_file("input.txt")
+    # print(f"\n# Answer: {answer}")
+    pass
+
+
+def test_part_two():
+    pass
+
+
+def part_two():
+    # inputs = load_input_file("input.txt")
+    # print(f"\n# Answer: {answer}")
+    pass
+
+
+if __name__ == "__main__":
+    test_part_one()
+    # part_one()
+    # test_part_two()
+    # part_two()
