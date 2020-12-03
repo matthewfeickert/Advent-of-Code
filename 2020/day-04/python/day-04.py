edb447678ca06529c9a48a103d6d1c3de4b4b63a
Matthew Feickert <matthew.feickert@cern.ch>
2020-12-03 01:22:32 -0600

    Add day 4 template

---
+++ 2020/day-04/python/day-04.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+
+def load_input_file(path):
+    with open(path) as input_file:
+        return [line.strip() for line in input_file]
+
+
+test_data = [None]
+
+
+def test_part_one():
+    pass
+
+
+def part_one():
+    # inputs = load_input_file("input.txt")
+    # print(f"\n# Answer: {answer}")
+    pass
+
+
+def test_part_two():
+    pass
+
+
+def part_two():
+    # inputs = load_input_file("input.txt")
+    # print(f"\n# Answer: {answer}")
+    pass
+
+
+if __name__ == "__main__":
+    test_part_one()
+    # part_one()
+    # test_part_two()
+    # part_two()
