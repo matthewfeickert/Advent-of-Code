3e61bf11aa9b622ec9489eca7b2fabbfc7eed17d
Matthew Feickert <matthew.feickert@cern.ch>
2020-12-03 00:35:21 -0600

    revise for readability

--- 2020/day-03/python/day-03.py
+++ 2020/day-03/python/day-03.py
@@ -24,59 +24,53 @@ def load_input_file(path):
 # """
 
 
-def count_trees(data, horizontal_shift, vertical_shift, target="#"):
+def count_trees(data, horiz_shift, vert_shift, target="#"):
     cycle_length = len(data[0])
     cycle_postion = 0
     tree_list = []
-    for row in data[vertical_shift::vertical_shift]:
-        cycle_postion = (cycle_postion + horizontal_shift) % cycle_length
+    for row in data[vert_shift::vert_shift]:
+        cycle_postion = (cycle_postion + horiz_shift) % cycle_length
         tree_list.append(row[cycle_postion])
     return tree_list.count(target)
 
 
 def test_part_one():
     test_data = load_input_file("test_data.txt")
-    tree_count = count_trees(
-        test_data, horizontal_shift=3, vertical_shift=1, target="#"
-    )
+    tree_count = count_trees(test_data, horiz_shift=3, vert_shift=1)
     assert tree_count == 7
 
 
 def part_one():
     inputs = load_input_file("input.txt")
-    tree_count = count_trees(inputs, horizontal_shift=3, vertical_shift=1, target="#")
+    tree_count = count_trees(inputs, horiz_shift=3, vert_shift=1)
     print(f"\n# Number of trees encountered: {tree_count}")
 
 
 def test_part_two():
     test_data = load_input_file("test_data.txt")
     shift_list = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
-    tree_counts = []
-    for shifts in shift_list:
-        tree_counts.append(
-            count_trees(
-                test_data,
-                horizontal_shift=shifts[0],
-                vertical_shift=shifts[1],
-                target="#",
-            )
+    tree_counts = [
+        count_trees(
+            test_data,
+            horiz_shift=h_shift,
+            vert_shift=v_shift,
         )
+        for h_shift, v_shift in shift_list
+    ]
     assert reduce(mul, tree_counts) == 336
 
 
 def part_two():
     inputs = load_input_file("input.txt")
     shift_list = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
-    tree_counts = []
-    for shifts in shift_list:
-        tree_counts.append(
-            count_trees(
-                inputs,
-                horizontal_shift=shifts[0],
-                vertical_shift=shifts[1],
-                target="#",
-            )
+    tree_counts = [
+        count_trees(
+            inputs,
+            horiz_shift=h_shift,
+            vert_shift=v_shift,
         )
+        for h_shift, v_shift in shift_list
+    ]
     answer = reduce(mul, tree_counts)
     print(f"\n# Product of trees encountered: {answer}")
 
