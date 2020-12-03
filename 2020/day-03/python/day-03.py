e7b8803cb99abd34e930fb9a785f968bacf353b4
Matthew Feickert <matthew.feickert@cern.ch>
2020-12-02 23:33:05 -0600

    Add part 1 solution

--- 2020/day-03/python/day-03.py
+++ 2020/day-03/python/day-03.py
@@ -1,22 +1,54 @@
 #!/usr/bin/env python3
 
+# import numpy as np
+
+# Notes:
+# Open: .
+# Tree: #
+
+# Slope: right 3, down 1
+
 
 def load_input_file(path):
     with open(path) as input_file:
         return [line.strip() for line in input_file]
 
 
-test_data = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]
+# test_data = """\
+# ..##.......
+# #...#...#..
+# .#....#..#.
+# ..#.#...#.#
+# .#...##..#.
+# ..#.##.....
+# .#.#.#....#
+# .#........#
+# #.##...#...
+# #...##....#
+# .#..#...#.#\
+# """
+
+
+def count_trees(data, shift, target="#"):
+    cycle_length = len(data[0])
+    cycle_postion = 0
+    tree_list = []
+    for row in data[1:]:
+        cycle_postion = (cycle_postion + shift) % cycle_length
+        tree_list.append(row[cycle_postion])
+    return tree_list.count(target)
 
 
 def test_part_one():
-    pass
+    test_data = load_input_file("test_data.txt")
+    tree_count = count_trees(test_data, shift=3, target="#")
+    assert tree_count == 7
 
 
 def part_one():
-    # inputs = load_input_file("input.txt")
-    # print(f"\n# Answer: {answer}")
-    pass
+    inputs = load_input_file("input.txt")
+    tree_count = count_trees(inputs, shift=3, target="#")
+    print(f"\n# Number of trees encountered: {tree_count}")
 
 
 def test_part_two():
@@ -31,6 +63,6 @@ def part_two():
 
 if __name__ == "__main__":
     test_part_one()
-    # part_one()
+    part_one()
     # test_part_two()
     # part_two()
