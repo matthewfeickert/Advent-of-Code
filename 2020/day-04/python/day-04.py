5ffdc75b859f27c5553e29c1409dbbda7ab5febf
Matthew Feickert <matthew.feickert@cern.ch>
2020-12-04 00:59:10 -0600

    Add invalid part 2

--- 2020/day-04/python/day-04.py
+++ 2020/day-04/python/day-04.py
@@ -4,7 +4,7 @@
 def load_input_file(path):
     with open(path) as input_file:
         entrys = input_file.read().split("\n\n")
-        return [entry.replace("\n", " ") for entry in entrys]
+        return [entry.replace("\n", " ").rstrip() for entry in entrys]
 
 
 test_data = """\
@@ -28,47 +28,107 @@ required_keys = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
 
 
 def create_passport(inputs):
-    passport_keys = [
-        [pair.split(":")[0] for pair in input.split(" ")] for input in inputs
-    ]
-    return passport_keys
+    # passport_keys = [
+    #     [pair.split(":")[0] for pair in input.split(" ")] for input in inputs
+    # ]
+    # passports = [{pair.split(":")[0],pair.split(":")[1] for pair in input.split(" ")} for input in inputs]
+    passports = []
+    for input in inputs:
+        passport = {}
+        for pair in input.split(" "):
+            key, value = pair.split(":")
+            passport[key] = value
+        passports.append(passport)
+    return passports
 
 
 def validate_passport(passports, required_keys=required_keys):
     valid_passports = [
-        all(keys in passport_keys for keys in required_keys)
-        for passport_keys in passports
+        all(keys in passport.keys() for keys in required_keys) for passport in passports
     ]
+    return valid_passports
+
+
+def validate_keys(passports):
+    valid_passports = []
+    for port in passports:
+        if int(port["byr"]) < 1920 or int(port["byr"]) > 2002:
+            continue
+        if int(port["iyr"]) < 2010 or int(port["iyr"]) > 2020:
+            continue
+        if int(port["eyr"]) < 2020 or int(port["eyr"]) > 2030:
+            continue
+        if port["hgt"][-2:] not in ["cm", "in"]:
+            continue
+        if port["hgt"][-2:] == "cm":
+            height = int(port["hgt"][:-2])
+            if height < 150 or height > 193:
+                continue
+        if port["hgt"][-2:] == "in":
+            height = int(port["hgt"][:-2])
+            if height < 59 or height > 76:
+                continue
+        hair_color = port["hcl"]
+        if len(hair_color) != 7:
+            continue
+        else:
+            if hair_color[0] != "#":
+                continue
+        if port["ecl"] not in ["amd", "blu", "brn", "gry", "grn", "hzl", "oth"]:
+            continue
+        if len(port["pid"]) != 9:
+            continue
+        if not port["pid"].isdecimal():
+            continue
+        valid_passports.append(port)
     return valid_passports
 
 
 def test_part_one():
     inputs = load_input_file("test_data.txt")
-    passport_keys = create_passport(inputs)
-    valid_passports = validate_passport(passport_keys)
+    passports = create_passport(inputs)
+    valid_passports = validate_passport(passports)
     assert valid_passports == [True, False, True, False]
     assert sum(valid_passports) == 2
 
 
 def part_one():
     inputs = load_input_file("input.txt")
-    passport_keys = create_passport(inputs)
-    valid_passports = validate_passport(passport_keys)
+    passports = create_passport(inputs)
+    valid_passports = validate_passport(passports)
     print(f"\n# Number of valid passports: {sum(valid_passports)}")
 
 
 def test_part_two():
-    pass
+    inputs = load_input_file("test_invalid.txt")
+    passports = create_passport(inputs)
+    valid_passports = [
+        x for idx, x in enumerate(passports) if validate_passport(passports)[idx]
+    ]
+    valid_passports = validate_keys(valid_passports)
+    assert len(valid_passports) == 0
+
+    inputs = load_input_file("test_valid.txt")
+    passports = create_passport(inputs)
+    valid_passports = [
+        x for idx, x in enumerate(passports) if validate_passport(passports)[idx]
+    ]
+    valid_passports = validate_keys(valid_passports)
+    assert len(valid_passports) == 4
 
 
 def part_two():
-    # inputs = load_input_file("input.txt")
-    # print(f"\n# Answer: {answer}")
-    pass
+    inputs = load_input_file("input.txt")
+    passports = create_passport(inputs)
+    valid_passports = [
+        x for idx, x in enumerate(passports) if validate_passport(passports)[idx]
+    ]
+    valid_passports = validate_keys(valid_passports)
+    print(f"\n# Number of valid passports: {len(valid_passports)}")
 
 
 if __name__ == "__main__":
     test_part_one()
     part_one()
-    # test_part_two()
-    # part_two()
+    test_part_two()
+    part_two()
