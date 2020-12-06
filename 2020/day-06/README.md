ef3635595af8414c7e414a5ac0b559492f253118
Matthew Feickert <matthew.feickert@cern.ch>
2020-12-05 23:19:04 -0600

    Add part one solution

--- 2020/day-06/README.md
+++ 2020/day-06/README.md
@@ -18,7 +18,7 @@ abcy
 abcz
 ```
 
-In this group, there are 6 questions to which anyone answered "yes": a, b, c, x, y, and z.
+In this group, there are 6 questions to which anyone answered "yes": `a`, `b`, `c`, `x`, `y`, and `z`.
 (Duplicate answers to the same question don't count extra; each question counts at most once.)
 
 Another group asks for your help, then another, and eventually you've collected answers from every group on the plane (your puzzle input).
--- 2020/day-06/python/day-06.py
+++ 2020/day-06/python/day-06.py
@@ -1,19 +1,45 @@
 def load_input_file(path):
     with open(path) as input_file:
-        return [line.strip() for line in input_file]
+        entrys = input_file.read().split("\n\n")
+        return [entry.replace("\n", "").rstrip() for entry in entrys]
 
 
-test_data = [None]
+test_data = """\
+abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b\
+"""
+
+
+def get_unique(data):
+    return "".join(sorted(list(set(data))))
 
 
 def test_part_one():
-    pass
+    inputs = load_input_file("test_data.txt")
+    sets = [get_unique(input) for input in inputs]
+    unique_answers = [len(answers) for answers in sets]
+    assert unique_answers == [3, 3, 3, 1, 1]
+    assert sum(unique_answers) == 11
 
 
 def part_one():
-    # inputs = load_input_file("input.txt")
-    # print(f"\n# Answer: {answer}")
-    pass
+    inputs = load_input_file("input.txt")
+    sets = [get_unique(input) for input in inputs]
+    unique_answers = [len(answers) for answers in sets]
+    print(f"\n# Answer: {sum(unique_answers)}")
 
 
 def test_part_two():
@@ -28,6 +54,6 @@ def part_two():
 
 if __name__ == "__main__":
     test_part_one()
-    # part_one()
+    part_one()
     # test_part_two()
     # part_two()
