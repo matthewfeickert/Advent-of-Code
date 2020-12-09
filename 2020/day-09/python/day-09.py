9c1ac614b0ea0e144bcf62dca14bb936a231635d
Matthew Feickert <matthew.feickert@cern.ch>
2020-12-09 00:48:06 -0600

    Add faster solution

--- 2020/day-09/python/day-09.py
+++ 2020/day-09/python/day-09.py
@@ -1,13 +1,9 @@
 from itertools import permutations
-from itertools import combinations
 
 
 def load_input_file(path):
     with open(path) as input_file:
         return [int(line.strip()) for line in input_file]
-
-
-test_data = [None]
 
 
 def compute(inputs, preamble=None):
@@ -27,30 +23,34 @@ def test_part_one():
 def part_one():
     inputs = load_input_file("input.txt")
     answer = compute(inputs, preamble=25)
-    print(f"\n# Answer: {answer}")
+    print(f"\n# First number not in sums of preamble: {answer}")
 
 
 def find_subrange(inputs, target):
-    idxs = list(range(len(inputs) + 1))
-    contiguous_sublists = [inputs[i:j] for i, j in combinations(idxs, 2)]
-    sublists = [list for list in contiguous_sublists if len(list) > 1]
-    for sublist in sublists:
-        if sum(sublist) == target:
-            return min(sublist) + max(sublist)
+    len_input = len(inputs)
+    for start in range(len_input):
+        for offset in range(len_input - start - 1):
+            subrange = inputs[start : start + offset]  # noqa: E203
+            if sum(subrange) == target:
+                return subrange
+
+
+def min_max_sum(data):
+    return min(data) + max(data)
 
 
 def test_part_two():
     inputs = load_input_file("test_data.txt")
     invalid_number = compute(inputs, preamble=5)
-    answer = find_subrange(inputs, invalid_number)
+    answer = min_max_sum(find_subrange(inputs, invalid_number))
     assert answer == 62
 
 
 def part_two():
     inputs = load_input_file("input.txt")
     invalid_number = compute(inputs, preamble=25)
-    answer = find_subrange(inputs, invalid_number)
-    print(f"\n# Answer: {answer}")
+    answer = min_max_sum(find_subrange(inputs, invalid_number))
+    print(f"\n# Sum of min and max of subrange: {answer}")
 
 
 if __name__ == "__main__":
