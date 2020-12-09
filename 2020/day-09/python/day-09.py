7f2adb42ce73ffad4aba3809ae6b686893c28b04
Matthew Feickert <matthew.feickert@cern.ch>
2020-12-09 00:05:55 -0600

    Add part 1 solution

--- 2020/day-09/python/day-09.py
+++ 2020/day-09/python/day-09.py
@@ -1,19 +1,32 @@
+from itertools import permutations
+
+
 def load_input_file(path):
     with open(path) as input_file:
-        return [line.strip() for line in input_file]
+        return [int(line.strip()) for line in input_file]
 
 
 test_data = [None]
 
 
+def compute(inputs, preamble=None):
+    for idx, item in enumerate(inputs[preamble:]):
+        available = inputs[idx : idx + preamble]  # noqa: E203
+        sum_of_pairs = [sum(pair) for pair in permutations(available, r=2)]
+        if not inputs[idx + preamble] in sum_of_pairs:
+            return inputs[idx + preamble]
+
+
 def test_part_one():
-    pass
+    inputs = load_input_file("test_data.txt")
+    answer = compute(inputs, preamble=5)
+    assert answer == 127
 
 
 def part_one():
-    # inputs = load_input_file("input.txt")
-    # print(f"\n# Answer: {answer}")
-    pass
+    inputs = load_input_file("input.txt")
+    answer = compute(inputs, preamble=25)
+    print(f"\n# Answer: {answer}")
 
 
 def test_part_two():
@@ -28,6 +41,6 @@ def part_two():
 
 if __name__ == "__main__":
     test_part_one()
-    # part_one()
+    part_one()
     # test_part_two()
     # part_two()
