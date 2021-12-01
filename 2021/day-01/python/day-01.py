a7edd8cad8a65063feaf221dedd98c68f7b2071a
Matthew Feickert <matthew.feickert@cern.ch>
2021-11-30 23:16:58 -0600

    Add part 1 solution

--- 2021/day-01/python/day-01.py
+++ 2021/day-01/python/day-01.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
-from itertools import combinations
-from functools import reduce
-from operator import mul
 
 
 def load_input_file(path):
@@ -9,38 +6,35 @@ def load_input_file(path):
         return [int(line.strip()) for line in input_file]
 
 
-test_data = [1721, 979, 366, 299, 675, 1456]
+test_data = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
 
 
-def product_of_ntuples(inputs, target, order):
-    # https://docs.python.org/3/library/itertools.html#itertools.combinations
-    for ntuple in combinations(inputs, order):
-        if sum(ntuple) == target:
-            return reduce(mul, ntuple)
+def count_increases(inputs):
+    return sum(inputs[step] > inputs[step - 1] for step in range(len(inputs)))
 
 
 def test_part_one():
-    assert product_of_ntuples(test_data, 2020, 2) == 514579
+    assert count_increases(test_data) == 7
 
 
 def part_one():
-    inputs = load_input_file("input.txt")
-    answer = product_of_ntuples(inputs, 2020, 2)
-    print(f"\n# Product of pair is: {answer}")
+    inputs = load_input_file("../input.txt")
+    answer = count_increases(inputs)
+    print(f"\n# Measurements larger than the previous measurement: {answer}")
 
 
-def test_part_two():
-    assert product_of_ntuples(test_data, 2020, 3) == 241861950
+# def test_part_two():
+#     assert product_of_ntuples(test_data, 2020, 3) == 241861950
 
 
-def part_two():
-    inputs = load_input_file("input.txt")
-    answer = product_of_ntuples(inputs, target=2020, order=3)
-    print(f"\n# Product of tripplet is: {answer}")
+# def part_two():
+#     inputs = load_input_file("input.txt")
+#     answer = product_of_ntuples(inputs, target=2020, order=3)
+#     print(f"\n# Product of tripplet is: {answer}")
 
 
 if __name__ == "__main__":
     test_part_one()
     part_one()
-    test_part_two()
-    part_two()
+    # test_part_two()
+    # part_two()
