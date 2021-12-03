64e8899ae5e77ecf549cf420c5b306b6a7010914
Matthew Feickert <matthew.feickert@cern.ch>
2021-12-03 00:46:57 -0600

    Add part 2

--- 2021/day-03/python/day-03.py
+++ 2021/day-03/python/day-03.py
@@ -29,7 +29,6 @@ def calculate_rates_in_decimal(inputs):
     delta = None
     bits = []
     for index in range(len(inputs[0])):
-        print(index)
         column = [x[index] for x in inputs]
         bits.append(max(column, key=column.count))
     epsilon = int("".join(bits), base=2)
@@ -52,18 +51,55 @@ def part_one():
     print(f"\n# Answer: {answer}")
 
 
+def oxygen_generation(inputs):
+    numbers = inputs.copy()
+    for index in range(len(numbers[0])):
+        column = [x[index] for x in numbers]
+
+        bit_key = "1" if column.count("1") >= column.count("0") else "0"
+
+        remove_list = [number for number in numbers if number[index] != bit_key]
+        for number in remove_list:
+            numbers.remove(number)
+        if len(numbers) == 1:
+            break
+
+    return int(numbers[0], base=2)
+
+
+def co2_scrubber(inputs):
+    numbers = inputs.copy()
+    for index in range(len(numbers[0])):
+        column = [x[index] for x in numbers]
+
+        bit_key = "1" if column.count("1") < column.count("0") else "0"
+
+        remove_list = [number for number in numbers if number[index] != bit_key]
+        for number in remove_list:
+            numbers.remove(number)
+        if len(numbers) == 1:
+            break
+
+    return int(numbers[0], base=2)
+
+
 def test_part_two():
-    pass
+    oxygen_rating = oxygen_generation(test_data)
+    co2_rating = co2_scrubber(test_data)
+    assert oxygen_rating, co2_rating == (23, 10)
+    assert oxygen_rating * co2_rating == 230
 
 
 def part_two():
-    # inputs = load_input_file("input.txt")
-    # print(f"\n# Answer: {answer}")
-    pass
+    inputs = load_input_file("input.txt")
+    oxygen_rating = oxygen_generation(inputs)
+    co2_rating = co2_scrubber(inputs)
+    answer = oxygen_rating * co2_rating
+    print(f"\n# Answer: {answer}")
 
 
 if __name__ == "__main__":
     test_part_one()
     part_one()
-    # test_part_two()
-    # part_two()
+    test_part_two()
+    part_two()
