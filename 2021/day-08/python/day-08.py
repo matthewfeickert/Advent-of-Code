683fc335b9f0d61a6319a28004eed715e63deba4
Matthew Feickert <matthew.feickert@cern.ch>
2021-12-07 23:26:46 -0600

    Add string processing

--- 2021/day-08/python/day-08.py
+++ 2021/day-08/python/day-08.py
@@ -1,5 +1,5 @@
 def process_input(input):
-    return input.splitlines()
+    return [[y.split(" ") for y in x.split(" | ")] for x in input.splitlines()]
 
 
 def load_input_file(path):
@@ -8,13 +8,26 @@ def load_input_file(path):
 
 
 test_string = """\
-01010\
+be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce\
 """
 test_data = process_input(test_string)
 
 
+def count_unique(inputs):
+    return 1
+
+
 def test_part_one():
-    pass
+    assert count_unique(test_data) == 26
 
 
 def part_one():
