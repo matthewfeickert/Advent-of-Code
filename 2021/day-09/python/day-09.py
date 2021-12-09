5f309fd4d732c243d2fcf220f9f6fa9dc7d96671
Matthew Feickert <matthew.feickert@cern.ch>
2021-12-08 23:42:47 -0600

    Add part 1 lazy solution

--- 2021/day-09/python/day-09.py
+++ 2021/day-09/python/day-09.py
@@ -1,5 +1,5 @@
 def process_input(input):
-    return input.splitlines()
+    return [list(map(int, list(x))) for x in input.splitlines()]
 
 
 def load_input_file(path):
@@ -8,19 +8,44 @@ def load_input_file(path):
 
 
 test_string = """\
-01010\
+2199943210
+3987894921
+9856789892
+8767896789
+9899965678\
 """
 test_data = process_input(test_string)
 
 
+def risk_level(inputs):
+    n_rows = len(inputs)
+    n_columns = len(inputs[0])
+    low_points = []
+
+    for row_idx, row in enumerate(inputs):
+        for idx, number in enumerate(row):
+            if idx > 0 and row[idx - 1] <= number:
+                continue
+            if idx < n_columns - 1 and row[idx + 1] <= number:
+                continue
+            if row_idx > 0 and inputs[row_idx - 1][idx] <= number:
+                continue
+            if row_idx < n_rows - 1 and inputs[row_idx + 1][idx] <= number:
+                continue
+            low_points.append(number)
+
+    risk_penalty = 1
+    return sum(height + risk_penalty for height in low_points)
+
+
 def test_part_one():
-    pass
+    assert risk_level(test_data) == 15
 
 
 def part_one():
-    # inputs = load_input_file("input.txt")
-    # print(f"\n# Answer: {answer}")
-    pass
+    inputs = load_input_file("input.txt")
+    answer = risk_level(inputs)
+    print(f"\n# Answer: {answer}")
 
 
 def test_part_two():
@@ -35,6 +60,6 @@ def part_two():
 
 if __name__ == "__main__":
     test_part_one()
-    # part_one()
+    part_one()
     # test_part_two()
     # part_two()
