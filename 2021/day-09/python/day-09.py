f6f8083044c247ea2a837a1eca0fbb1254dd7b8a
Matthew Feickert <matthew.feickert@cern.ch>
2021-12-09 00:29:58 -0600

    freeze

--- 2021/day-09/python/day-09.py
+++ 2021/day-09/python/day-09.py
@@ -48,8 +48,40 @@ def part_one():
     print(f"\n# Answer: {answer}")
 
 
+def get_basins(inputs):
+    n_rows = len(inputs)
+    n_columns = len(inputs[0])
+    basins = []
+
+    nine_index_map = []
+
+    for row_idx, row in enumerate(inputs):
+        for idx, number in enumerate(row):
+            if number == 9:
+                nine_index_map.append([row_idx, idx])
+
+    n_positions = len(nine_index_map)
+    basin = []
+    used_positions = []
+    for idx, position in enumerate(nine_index_map):
+        # get horizontal distance to next 9
+        if idx < n_positions and position not in used_positions:
+            if position[0] == nine_index_map[idx + 1][0]:
+                row_idx = position[0]
+                # check if there is any area to fill
+                if nine_index_map[idx + 1][1] - position[1] > 1:
+                    this_idx = position[1]
+                    next_idx = nine_index_map[idx + 1][1]
+                    basin += inputs[row_idx][this_idx + 1 : next_idx]
+                    used_positions.append(position)
+                    used_positions.append(nine_index_map[idx + 1])
+                    print("Another 9 on same row")
+                    print(position, nine_index_map[idx + 1])
+                    breakpoint()
+
+
 def test_part_two():
-    pass
+    assert get_basins(test_data) == 1134
 
 
 def part_two():
@@ -61,5 +93,5 @@ def part_two():
 if __name__ == "__main__":
     test_part_one()
     part_one()
-    # test_part_two()
+    test_part_two()
     # part_two()
